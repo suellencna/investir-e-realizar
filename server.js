c127fdd842679a83721c44ec4940a82c8af55f32
Suellen Andrade Pinto <suellencna@gmail.com>
2026-09-15 10:24:12 -0300

    Corrige rota principal - desabilita index automático do express.static

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 // Página principal = Consultoria
 app.get('/', (req, res) => {
